574aa0a019123c583ae91fecfb93bf75f468d5e1
Benson-liu-fan <15875466405@163.com>
2020-05-18 23:31:33 +0800

    猜数游戏.py

---
+++ 猜数游戏.py
@@ -0,0 +1,19 @@
+import random
+x = random.randint(0,30)
+n = input()
+m = 0
+while n.isalpha() == False:            #判断是否为字符
+    m += 1
+    if eval(n) > x:
+        print("遗憾，太大了！")
+    elif eval(n) < x:
+        print("遗憾，太小了！")
+    else:
+        print("预测{}次，你猜中了！".format(m))
+        break
+    n = input()
+    if n.isalpha() == True:
+        print("请输入一个数字！")
+        break
+else:
+    print("请输入一个数字！")
